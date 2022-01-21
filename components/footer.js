009b1310721a636da504a4e17abd252bb6c6f617
Omkar Londhe <87084951+Omkaar1@users.noreply.github.com>
2022-01-21 23:55:40 +0530

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,56 @@
+function footer() {
+  return `<div id="footer_div">
+  <div id="footerBox1">
+    <p>Shop Non-Stop on Meesho</p>
+    <p>
+      Trusted by more than 1 Crore <br />
+      Indians Cash on Delivery | Free Delivery
+    </p>
+    <div id="footerGP">
+      <img
+        src="https://meesho.com/_next/static/images/google-play-button-1f2aa747a3eefa161840b04e057b31ac.png"
+        alt=""
+      />
+      <img
+        src="https://meesho.com/_next/static/images/appstore-button-4b171cf04fe0557718dfd2cbf309c61d.png"
+        alt=""
+      />
+    </div>
+  </div>
+  <div id="footerBox2">
+    <div id="ftBox2">
+      <div>
+        <p>Careers</p>
+        <p>Become a supplier</p>
+      </div>
+      <div>
+        <p>Legal & Policies</p>
+        <p>Meesho Tech Blog</p>
+      </div>
+      <div>
+        <h2>Contact Us</h2>
+        <p>query@meesho.com</p>
+      </div>
+      <div>
+        <h2>Reach out to us</h2>
+        <div id="contact_logo">
+          <img src="https://meesho.com/assets/facebook.png" alt="" />
+          <img src="https://meesho.com/assets/instagram.png" alt="" />
+          <img src="https://meesho.com/assets/youtube.png" alt="" />
+          <img src="https://meesho.com/assets/linkedin.png" alt="" />
+          <img src="https://meesho.com/assets/twitter.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div id="msg_div">
+      <p>
+        If you have received a product that you did not order, please call
+        us on 080-68878782. Please note no other support will be provided
+        here.
+      </p>
+    </div>
+  </div>
+</div>`;
+}
+
+export default footer;
